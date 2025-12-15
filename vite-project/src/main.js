9e5387677a21abc0487564ba7fa227e40c39382c
vladimirovicp <vladimirovicp70@gmail.com>
2025-12-15 12:12:43 +0400

    feat: process 2

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,24 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+import htmlacademy from './htmlacademy.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    <a href="https://htmlacademy.ru/" target="_blank">
+      <img src="${htmlacademy}" class="logo vanilla" alt="Htmlacademy logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
